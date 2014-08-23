8c0c54dc124ba7f82a0aaf648eb7a3012da43839
Christian Gendreau <christiangendreau@gmail.com>
2014-08-23 18:10:53 -0400

    missing file

---
+++ dwca-validator-lib/src/test/java/org/gbif/dwc/validator/TestEvaluationResultHelper.java
@@ -0,0 +1,55 @@
+package org.gbif.dwc.validator;
+
+import org.gbif.dwc.validator.result.ValidationResult;
+import org.gbif.dwc.validator.result.ValidationResultElement;
+import org.gbif.dwc.validator.result.type.ValidationTypeIF;
+
+import java.util.List;
+
+
+public class TestEvaluationResultHelper {
+
+
+  /**
+   * Check that a message associated with an id is in the result list.
+   * 
+   * @param results
+   * @param id
+   * @param message
+   * @return
+   */
+  public static boolean containsResultMessage(List<ValidationResult> results, String id, String message) {
+    for (ValidationResult currResult : results) {
+      if (currResult.getId().equals(id)) {
+        for (ValidationResultElement currValidationElement : currResult.getResults()) {
+          if (message.equals(currValidationElement.getExplanation())) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Check that a validationType associated with an id is in the result list.
+   * 
+   * @param results
+   * @param validationType
+   * @return
+   */
+  public static boolean containsValidationType(List<ValidationResult> results, String id,
+    ValidationTypeIF validationType) {
+    for (ValidationResult currResult : results) {
+      if (currResult.getId().equals(id)) {
+        for (ValidationResultElement currValidationElement : currResult.getResults()) {
+          if (validationType.equals(currValidationElement.getType())) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+}
