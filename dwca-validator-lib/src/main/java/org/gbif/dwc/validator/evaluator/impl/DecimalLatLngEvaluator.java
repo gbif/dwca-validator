8de0c1a2bf209b83d3d465ec8c8971556f51bd08
Christian Gendreau <christiangendreau@gmail.com>
2014-08-23 18:09:15 -0400

    initial DecimaleLatLngEvaluator

---
+++ dwca-validator-lib/src/main/java/org/gbif/dwc/validator/evaluator/impl/DecimalLatLngEvaluator.java
@@ -0,0 +1,155 @@
+package org.gbif.dwc.validator.evaluator.impl;
+
+import org.gbif.dwc.record.Record;
+import org.gbif.dwc.terms.DwcTerm;
+import org.gbif.dwc.validator.config.ArchiveValidatorConfig;
+import org.gbif.dwc.validator.evaluator.RecordEvaluatorIF;
+import org.gbif.dwc.validator.evaluator.annotation.RecordEvaluator;
+import org.gbif.dwc.validator.result.EvaluationContext;
+import org.gbif.dwc.validator.result.Result;
+import org.gbif.dwc.validator.result.ResultAccumulatorIF;
+import org.gbif.dwc.validator.result.ValidationResult;
+import org.gbif.dwc.validator.result.ValidationResultElement;
+import org.gbif.dwc.validator.result.type.ContentValidationType;
+import org.gbif.dwc.validator.rule.value.NumericalValueEvaluationRule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Decimal latitude/longitude evaluator.
+ * 
+ * @author cgendreau
+ */
+@RecordEvaluator(key = "decimalLatLngEvaluator")
+public class DecimalLatLngEvaluator implements RecordEvaluatorIF {
+
+  public static class DecimalLatLngEvaluatorBuilder {
+
+    private final String key = DecimalLatLngEvaluator.class.getAnnotation(RecordEvaluator.class).key();
+    private NumericalValueEvaluationRule latNumericalValueEvaluationRule;
+    private NumericalValueEvaluationRule lngNumericalValueEvaluationRule;
+
+    private DecimalLatLngEvaluatorBuilder() {
+      // build lat/long default NumericalValueEvaluationRule
+      latNumericalValueEvaluationRule =
+        NumericalValueEvaluationRule.createRule().boundedBy(MIN_LATITUDE, MAX_LATITUDE).build();
+      lngNumericalValueEvaluationRule =
+        NumericalValueEvaluationRule.createRule().boundedBy(MIN_LONGITUDE, MAX_LONGITUDE).build();
+    }
+
+    /**
+     * Create with default value. Using coreId, ValidationContext.CORE
+     * 
+     * @return
+     */
+    public static DecimalLatLngEvaluatorBuilder create() {
+      return new DecimalLatLngEvaluatorBuilder();
+    }
+
+    /**
+     * Build DecimalLatLngEvaluator.
+     * 
+     * @return immutable DecimalLatLngEvaluator object
+     */
+    public DecimalLatLngEvaluator build() {
+      return new DecimalLatLngEvaluator(key, latNumericalValueEvaluationRule, lngNumericalValueEvaluationRule);
+    }
+
+    /**
+     * Change the default NumericalValueEvaluationRule used to evaluate the value of lat/long fields.
+     * This could be used to restrict the accepted extent.
+     * 
+     * @param latNumericalValueEvaluationRule
+     * @param lngNumericalValueEvaluationRule
+     */
+    public void using(NumericalValueEvaluationRule latNumericalValueEvaluationRule,
+      NumericalValueEvaluationRule lngNumericalValueEvaluationRule) {
+      this.latNumericalValueEvaluationRule = latNumericalValueEvaluationRule;
+      this.lngNumericalValueEvaluationRule = lngNumericalValueEvaluationRule;
+    }
+
+  }
+
+  private final String key;
+
+  // decimalLatitude : Legal values lie between -90 and 90, inclusive.
+  public static final double MIN_LATITUDE = -90d;
+  public static final double MAX_LATITUDE = 90d;
+
+  // decimalLongitude : Legal values lie between -180 and 180, inclusive.
+  public static final double MIN_LONGITUDE = -180d;
+  public static final double MAX_LONGITUDE = 180d;
+
+  private final NumericalValueEvaluationRule latNumericalValueEvaluationRule;
+  private final NumericalValueEvaluationRule lngNumericalValueEvaluationRule;
+
+  public DecimalLatLngEvaluator(String key, NumericalValueEvaluationRule latNumericalValueEvaluationRule,
+    NumericalValueEvaluationRule lngNumericalValueEvaluationRule) {
+    this.key = key;
+    this.latNumericalValueEvaluationRule = latNumericalValueEvaluationRule;
+    this.lngNumericalValueEvaluationRule = lngNumericalValueEvaluationRule;
+  }
+
+  @Override
+  public String getKey() {
+    return key;
+  }
+
+  @Override
+  public void handleEval(Record record, ResultAccumulatorIF resultAccumulator) {
+    String lat = record.value(DwcTerm.decimalLatitude);
+    String lng = record.value(DwcTerm.decimalLongitude);
+    String id = record.id();
+
+    // if both are empty simply return, nothing to evaluate.
+    if (StringUtils.isBlank(lat) && StringUtils.isBlank(lng)) {
+      return;
+    }
+
+    // validate if it's a number and within the defined bounds
+    ValidationResultElement latResultElement = latNumericalValueEvaluationRule.evaluate(lat);
+    ValidationResultElement lngResultElement = lngNumericalValueEvaluationRule.evaluate(lng);
+
+    if (latResultElement != null || lngResultElement != null) {
+      List<ValidationResultElement> validationResultElementList = new ArrayList<ValidationResultElement>();
+      if (latResultElement != null) {
+        validationResultElementList.add(latResultElement);
+      }
+      if (lngResultElement != null) {
+        validationResultElementList.add(lngResultElement);
+      }
+
+      // try to swap lat and lng and re-evaluate
+      if (latNumericalValueEvaluationRule.evaluate(lng) == null
+        && lngNumericalValueEvaluationRule.evaluate(lat) == null) {
+        validationResultElementList.add(new ValidationResultElement(ContentValidationType.RECORD_CONTENT_VALUE,
+          Result.WARNING, ArchiveValidatorConfig.getLocalizedString("evaluator.decimal_lat_lng.inverted", lat, lng)));
+      }
+      resultAccumulator.accumulate(new ValidationResult(id, key, EvaluationContext.CORE, validationResultElementList));
+      // stop here since at least one NumericalValueEvaluation rule failed
+      return;
+    }
+
+    Double dLat = Double.parseDouble(lat);
+    Double dLng = Double.parseDouble(lng);
+
+    if (dLat.doubleValue() == 0d || dLng.doubleValue() == 0d) {
+      resultAccumulator.accumulate(new ValidationResult(id, key, EvaluationContext.CORE, new ValidationResultElement(
+        ContentValidationType.RECORD_CONTENT_VALUE, Result.WARNING, ArchiveValidatorConfig.getLocalizedString(
+          "evaluator.decimal_lat_lng.zero", lat, lng))));
+    }
+
+    // TODO validate precision, number of digits
+
+
+  }
+
+  @Override
+  public void handlePostIterate(ResultAccumulatorIF resultAccumulator) {
+    // no op
+  }
+
+}
---
+++ dwca-validator-lib/src/test/java/org/gbif/dwc/validator/evaluator/DecimalLatLngEvaluatorTest.java
@@ -0,0 +1,103 @@
+package org.gbif.dwc.validator.evaluator;
+
+import org.gbif.dwc.record.Record;
+import org.gbif.dwc.record.RecordImpl;
+import org.gbif.dwc.terms.DwcTerm;
+import org.gbif.dwc.text.ArchiveField;
+import org.gbif.dwc.text.ArchiveField.DataType;
+import org.gbif.dwc.validator.TestEvaluationResultHelper;
+import org.gbif.dwc.validator.config.ArchiveValidatorConfig;
+import org.gbif.dwc.validator.evaluator.impl.DecimalLatLngEvaluator;
+import org.gbif.dwc.validator.evaluator.impl.DecimalLatLngEvaluator.DecimalLatLngEvaluatorBuilder;
+import org.gbif.dwc.validator.result.impl.InMemoryResultAccumulator;
+import org.gbif.dwc.validator.result.type.ContentValidationType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class DecimalLatLngEvaluatorTest {
+
+  public static void main(String[] args) {
+    System.out.println(System.currentTimeMillis());
+  }
+
+  private Record buildMockRecord(String id, String lat, String lng) {
+    // create a mock Record
+    ArchiveField idField = new ArchiveField(0, DwcTerm.occurrenceID, null, DataType.string);
+    ArchiveField latitudeField = new ArchiveField(1, DwcTerm.decimalLatitude, null, DataType.string);
+    ArchiveField longitudeField = new ArchiveField(2, DwcTerm.decimalLongitude, null, DataType.string);
+    List<ArchiveField> fieldList = new ArrayList<ArchiveField>();
+    fieldList.add(latitudeField);
+    fieldList.add(longitudeField);
+    RecordImpl testRecord = new RecordImpl(idField, fieldList, "rowType", false);
+    testRecord.setRow(new String[] {id, lat, lng});
+    return testRecord;
+  }
+
+  @Test
+  public void testInvalidCoordinates() {
+    InMemoryResultAccumulator resultAccumulator = new InMemoryResultAccumulator();
+    DecimalLatLngEvaluator decimalLatLngEvaluator = DecimalLatLngEvaluatorBuilder.create().build();
+
+    decimalLatLngEvaluator.handleEval(buildMockRecord("1", "a", "40"), resultAccumulator);
+    decimalLatLngEvaluator.handleEval(buildMockRecord("2", "70", "b"), resultAccumulator);
+
+    assertTrue(TestEvaluationResultHelper.containsValidationType(resultAccumulator.getValidationResultsList(), "1",
+      ContentValidationType.RECORD_CONTENT_VALUE));
+    assertTrue(TestEvaluationResultHelper.containsValidationType(resultAccumulator.getValidationResultsList(), "2",
+      ContentValidationType.RECORD_CONTENT_VALUE));
+  }
+
+  @Test
+  public void testOutOfBoundsCoordinates() {
+
+    InMemoryResultAccumulator resultAccumulator = new InMemoryResultAccumulator();
+    DecimalLatLngEvaluator decimalLatLngEvaluator = DecimalLatLngEvaluatorBuilder.create().build();
+
+    decimalLatLngEvaluator.handleEval(buildMockRecord("1", "91", "120"), resultAccumulator);
+    decimalLatLngEvaluator.handleEval(buildMockRecord("2", "91", "40"), resultAccumulator);
+    decimalLatLngEvaluator.handleEval(buildMockRecord("3", "70", "181"), resultAccumulator);
+
+    assertTrue(TestEvaluationResultHelper.containsValidationType(resultAccumulator.getValidationResultsList(), "1",
+      ContentValidationType.RECORD_CONTENT_BOUNDS));
+    assertTrue(TestEvaluationResultHelper.containsValidationType(resultAccumulator.getValidationResultsList(), "2",
+      ContentValidationType.RECORD_CONTENT_BOUNDS));
+    assertTrue(TestEvaluationResultHelper.containsValidationType(resultAccumulator.getValidationResultsList(), "3",
+      ContentValidationType.RECORD_CONTENT_BOUNDS));
+  }
+
+  @Test
+  public void testPossiblyInvalidCoordinates() {
+    InMemoryResultAccumulator resultAccumulator = new InMemoryResultAccumulator();
+    DecimalLatLngEvaluator decimalLatLngEvaluator = DecimalLatLngEvaluatorBuilder.create().build();
+
+    decimalLatLngEvaluator.handleEval(buildMockRecord("1", "-120", "40"), resultAccumulator);
+    decimalLatLngEvaluator.handleEval(buildMockRecord("2", "0", "0"), resultAccumulator);
+
+    assertTrue(TestEvaluationResultHelper.containsResultMessage(resultAccumulator.getValidationResultsList(), "1",
+      ArchiveValidatorConfig.getLocalizedString("evaluator.decimal_lat_lng.inverted", "-120", "40")));
+    assertTrue(TestEvaluationResultHelper.containsValidationType(resultAccumulator.getValidationResultsList(), "1",
+      ContentValidationType.RECORD_CONTENT_VALUE));
+
+    assertTrue(TestEvaluationResultHelper.containsResultMessage(resultAccumulator.getValidationResultsList(), "2",
+      ArchiveValidatorConfig.getLocalizedString("evaluator.decimal_lat_lng.zero", "0", "0")));
+  }
+
+  @Test
+  public void testValidCoordinates() {
+
+    InMemoryResultAccumulator resultAccumulator = new InMemoryResultAccumulator();
+    DecimalLatLngEvaluator decimalLatLngEvaluator = DecimalLatLngEvaluatorBuilder.create().build();
+
+    decimalLatLngEvaluator.handleEval(buildMockRecord("1", "30.001", "40.001"), resultAccumulator);
+    decimalLatLngEvaluator.handleEval(buildMockRecord("2", "30", "120"), resultAccumulator);
+
+    assertTrue(resultAccumulator.getValidationResultsList().isEmpty());
+  }
+
+}
