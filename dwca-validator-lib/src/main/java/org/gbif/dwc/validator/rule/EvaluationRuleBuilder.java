f4d744d53645f9209edcd8372cb2165a142b55ce
Christian Gendreau <christiangendreau@gmail.com>
2014-11-13 13:42:54 -0500

    prepare for rules building refactoring

---
+++ dwca-validator-lib/src/main/java/org/gbif/dwc/validator/rule/EvaluationRuleBuilder.java
@@ -0,0 +1,17 @@
+package org.gbif.dwc.validator.rule;
+
+/**
+ * Interface defining a EvaluationRuleIF<String> builder.
+ * 
+ * @author cgendreau
+ */
+public interface EvaluationRuleBuilder {
+
+  /**
+   * Build a concrete instance of EvaluationRuleIF<String>.
+   * 
+   * @return
+   */
+  EvaluationRuleIF<String> build();
+
+}
---
+++ dwca-validator-lib/src/main/java/org/gbif/dwc/validator/rule/annotation/EvaluationRuleBuilderKey.java
@@ -0,0 +1,17 @@
+package org.gbif.dwc.validator.rule.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation used to identify EvaluationRule builder class.
+ * 
+ * @author cgendreau
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface EvaluationRuleBuilderKey {
+
+}
