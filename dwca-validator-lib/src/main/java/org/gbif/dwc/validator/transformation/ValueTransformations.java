60519ea22e044b64b0d3b39400f86ea9ee1e57a9
Christian Gendreau <christiangendreau@gmail.com>
2015-02-27 23:25:56 -0500

    remove PresenceValueTransformation

--- dwca-validator-lib/src/main/java/org/gbif/dwc/validator/transformation/ValueTransformations.java
+++ dwca-validator-lib/src/main/java/org/gbif/dwc/validator/transformation/ValueTransformations.java
@@ -2,8 +2,6 @@ package org.gbif.dwc.validator.transformation;
 
 import org.gbif.dwc.terms.Term;
 
-import java.util.List;
-
 import org.threeten.bp.temporal.TemporalAccessor;
 
 /**
@@ -16,20 +14,6 @@ public class ValueTransformations {
   public static ValueTransformation<Number> toNumeric(Term term) {
     return new NumericValueTransformation(term);
   }
-
-  public static ValueTransformation<Boolean> toPresence(Term term) {
-    return new PresenceValueTransformation(term);
-  }
-
-  /**
-   * @param term
-   * @param absenceSynonym list of Strings that should be considered as an absence e.g. "null", "na"
-   * @return
-   */
-  public static ValueTransformation<Boolean> toPresence(Term term, List<String> absenceSynonym) {
-    return new PresenceValueTransformation(term, absenceSynonym);
-  }
-
 
   public static ValueTransformation<TemporalAccessor> toISODate(Term term, boolean allowPartialDate,
     boolean allowMissingLeadingZeros) {
